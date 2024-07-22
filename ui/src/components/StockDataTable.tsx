920b4010d4f6b96d40675c9b8e054d72331ca244
nagendrakodama123 <126648606+nagendrakodama123@users.noreply.github.com>
2024-07-22 23:27:01 +0530

    delete unused code

--- ui/src/components/StockDataTable.tsx
+++ ui/src/components/StockDataTable.tsx
@@ -16,16 +16,18 @@ const StockDataTable: React.FC = () => {
 
     return (<div className="stock-table-container">
         <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <div style={{display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: "5px",
-    border: "1px dotted gray",
-    borderRadius: "50%"}} onClick={() => { dispatch(setSelectedStock(null)) }}> show all</div>
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                padding: "5px",
+                border: "1px dotted gray",
+                borderRadius: "50%"
+            }} onClick={() => { dispatch(setSelectedStock(null)) }}> show all</div>
             {imageData.map(image => (
-            <div style={{padding: '5px'}} key={image.stock_id} onClick={() => { dispatch(setSelectedStock(image.stock_id)) }}>
-                <img src={image.stock_image} alt={image.stock_id} style={{ width: '50px', height: '50px' }} />
-            </div>))}</div>
+                <div style={{ padding: '5px' }} key={image.stock_id} onClick={() => { dispatch(setSelectedStock(image.stock_id)) }}>
+                    <img src={image.stock_image} alt={image.stock_id} style={{ width: '50px', height: '50px' }} />
+                </div>))}</div>
         {data && Object.keys(data).length === 0 ? (
             <p>Loading data...</p>
         ) :
@@ -56,42 +58,6 @@ const StockDataTable: React.FC = () => {
         }
 
     </div>
-    );
-    return (
-        <div className="stock-table-container">
-            {/* {imageData.map(image => (
-                <div key={image.stock_id}>
-
-                    <div>
-                        <img src={image.stock_image} alt={image.stock_id} style={{ width: '50px', height: '50px' }} />
-                    </div>
-                </div>))} */}
-            {data && Object.keys(data).length === 0 ? (
-                <p>Loading data...</p>
-            ) : (
-                Object.keys(data).map(stockKey => (
-                    <div key={stockKey} className="stock-section">
-                        <h2>{stockKey}</h2>
-                        <table className="stock-table">
-                            <thead>
-                                <tr>
-                                    <th>Current Price</th>
-                                    <th>TimeStamp</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {/* {Array.isArray(data[stockKey]) && data[stockKey].map(stock => (
-                                    <tr key={stock._id}>
-                                        <td>{stock.current_price}</td>
-                                        <td>{new Date(stock.timeStamp).toLocaleString()}</td>
-                                    </tr>
-                                ))} */}
-                            </tbody>
-                        </table>
-                    </div>
-                ))
-            )}
-        </div>
     );
 }
 
